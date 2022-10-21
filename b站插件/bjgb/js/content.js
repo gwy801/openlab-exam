ea02a4adb54da84891a36452a9a6322283e5c112
gwy <gwy801@outlook.com>
2022-10-21 23:21:17 +0800

    first time

--- b站插件/bjgb/js/content.js
+++ b站插件/bjgb/js/content.js
@@ -18,8 +18,8 @@ var changeBKColor = function() {
     function createPage() {
         var div=document.createElement("div");
         var select=document.createElement("select");
-        select.innerHTML="<option value='1' selected>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='6'>6</option><option value='8'>8</option><option value='16'>16</option>";
-        select.setAttribute("style","width: 80px;padding: 5px;border-radius: 4px;background-color: #5af8eda6;font-weight: bolder;font-size: 15px;position: absolute;top: 60px;left: 10px;");
+        select.innerHTML="<option value='1' selected>1</option><option value='2'>2</option><option value='2.5'>2.5</option><option value='3'>3</option><option value='4'>4</option><option value='6'>6</option><option value='8'>8</option><option value='16'>16</option>";
+        select.setAttribute("style","width: 100px;padding: 10px;border-radius: 5px;background-color: green;font-weight: bolder;font-size: 20px;position: absolute;top: 60px;left: 10px;");
         select.setAttribute("onchange","document.getElementsByTagName('video')[0].playbackRate=this.value");
         div.appendChild(select);
         document.getElementsByTagName("body")[0].appendChild(div);
